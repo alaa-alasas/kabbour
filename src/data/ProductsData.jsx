7c07a52fb349bb946b58b74ea11c35a9a3f7d36d
alaa-alasas <erp.kabbour@gmail.com>
2025-05-26 15:47:46 +0300

    add mate images for testing

--- src/data/ProductsData.jsx
+++ src/data/ProductsData.jsx
@@ -1,9 +1,57 @@
 export const ProductsData = [
     {
-        img: "/kabbour/ProductDetails/Hibiscus.png",
+        img: "/kabbour/ProductDetails/kaharta-green.png",
         productId: 1,
         productName: "product.1.name",
         productDesc: "product.1.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/kaharta-green.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/kahartaLogo.png",
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/kaharta-white.png",
+        productId: 2,
+        productName: "product.2.name",
+        productDesc: "product.2.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/kaharta-white.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/kahartaLogo.png",
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/taragui.png",
+        productId: 3,
+        productName: "product.3.name",
+        productDesc: "product.3.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/taragui.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/taraguiLogo.png",
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/amanda.png",
+        productId: 4,
+        productName: "product.4.name",
+        productDesc: "product.4.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/amanda.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/amandaLogo.png",
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/Hibiscus.png",
+        productId: 5,
+        productName: "product.5.name",
+        productDesc: "product.5.desc",
         details:{
             images: [ 
                 "/kabbour/ProductDetails/Hibiscus.png",
@@ -11,29 +59,27 @@ export const ProductsData = [
                 "/kabbour/ProductDetails/cup.png",
             ],
             flowerImg: "/kabbour/ProductDetails/flower.png",
-
         }
     },
     {
         img: "/kabbour/ProductDetails/greenTea.png",
-        productId: 2,
-        productName: "product.2.name",
-        productDesc: "product.2.desc",
+        productId: 6,
+        productName: "product.6.name",
+        productDesc: "product.6.desc",
         details:{
             images: [ 
                 "/kabbour/ProductDetails/greenTea.png",
                 "/kabbour/ProductDetails/22.png",
                 "/kabbour/ProductDetails/greenTeaCup.png",
             ],
-            flowerImg: "/kabbour/ProductDetails/greenTeaFlower.png",
-            
+            flowerImg: "/kabbour/ProductDetails/greenTeaFlower.png",          
         }
     },
     {
         img: "/kabbour/ProductDetails/Minit.png",
-        productId: 3,
-        productName: "product.3.name", 
-        productDesc: "product.3.desc",
+        productId: 7,
+        productName: "product.7.name", 
+        productDesc: "product.7.desc",
         details:{
             images: [ 
                 "/kabbour/ProductDetails/Minit.png",
@@ -41,37 +87,32 @@ export const ProductsData = [
                 "/kabbour/ProductDetails/MinitCup.png",
             ],
             flowerImg: "/kabbour/ProductDetails/MinitFlower.png",
-            
         }
-
     },
     {
         img: "/kabbour/ProductDetails/Anise.jpeg",
-        productId: 4,
-        productName: "product.4.name", 
-        productDesc: "product.4.desc",
+        productId: 8,
+        productName: "product.8.name", 
+        productDesc: "product.8.desc",
         details:{
             images: [ 
                 "/kabbour/ProductDetails/Anise.jpeg",
                 "/kabbour/ProductDetails/22.png",
             ],
-            flowerImg: "/kabbour/ProductDetails/MinitFlower.png",
-            
+            flowerImg: "/kabbour/ProductDetails/MinitFlower.png",          
         }
     },
     {
         img: "/kabbour/ProductDetails/jouriFlowers2.jpeg",
-        productId: 5,
-        productName: "product.5.name", 
-        productDesc: "product.5.desc",
+        productId: 9,
+        productName: "product.9.name", 
+        productDesc: "product.9.desc",
         details:{
             images: [ 
                 "/kabbour/ProductDetails/jouriFlowers2.jpeg",
                 "/kabbour/ProductDetails/22.png",
             ],
             flowerImg: "/kabbour/ProductDetails/flower.png",
-            
         }
-
     }
 ];
