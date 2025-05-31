6062059352c97f9740afa9f58f46cbad210fc25a
alaa-alasas <erp.kabbour@gmail.com>
2025-05-31 15:45:13 +0300

    add Natural Herbs product images

--- src/data/ProductsData.jsx
+++ src/data/ProductsData.jsx
@@ -64,13 +64,14 @@ export const ProductsData = [
         }
     },
     {
-        img: "/kabbour/ProductDetails/Hibiscus.png",
+        img: "/kabbour/ProductDetails/Herbs/Hibiscus.png",
         productId: 5,
+        category: "Natural Herbs",
         productName: "product.5.name",
         productDesc: "product.5.desc",
         details:{
             images: [ 
-                "/kabbour/ProductDetails/Hibiscus.png",
+                "/kabbour/ProductDetails/Herbs/Hibiscus.png",
                 "/kabbour/ProductDetails/22.png",
                 "/kabbour/ProductDetails/cup.png",
             ],
@@ -82,13 +83,14 @@ export const ProductsData = [
         }
     },
     {
-        img: "/kabbour/ProductDetails/greenTea.png",
+        img: "/kabbour/ProductDetails/Herbs/GreenTea.png",
         productId: 6,
+        category: "Natural Herbs",
         productName: "product.6.name",
         productDesc: "product.6.desc",
         details:{
             images: [ 
-                "/kabbour/ProductDetails/greenTea.png",
+                "/kabbour/ProductDetails/Herbs/GreenTea.png",
                 "/kabbour/ProductDetails/22.png",
                 "/kabbour/ProductDetails/greenTeaCup.png",
             ],
@@ -100,13 +102,14 @@ export const ProductsData = [
         }
     },
     {
-        img: "/kabbour/ProductDetails/Minit.png",
+        img: "/kabbour/ProductDetails/Herbs/Mint.png",
         productId: 7,
+        category: "Natural Herbs",
         productName: "product.7.name", 
         productDesc: "product.7.desc",
         details:{
             images: [ 
-                "/kabbour/ProductDetails/Minit.png",
+                "/kabbour/ProductDetails/Herbs/Minit.png",
                 "/kabbour/ProductDetails/22.png",
                 "/kabbour/ProductDetails/MinitCup.png",
             ],
@@ -118,13 +121,14 @@ export const ProductsData = [
         }
     },
     {
-        img: "/kabbour/ProductDetails/Anise.jpeg",
+        img: "/kabbour/ProductDetails/Herbs/Anise.png",
         productId: 8,
+        category: "Natural Herbs",
         productName: "product.8.name", 
         productDesc: "product.8.desc",
         details:{
             images: [ 
-                "/kabbour/ProductDetails/Anise.jpeg",
+                "/kabbour/ProductDetails/Herbs/Anise.png",
                 "/kabbour/ProductDetails/22.png",
             ],
             flowerImg: "/kabbour/ProductDetails/MinitFlower.png",
@@ -135,13 +139,376 @@ export const ProductsData = [
         }
     },
     {
-        img: "/kabbour/ProductDetails/jouriFlowers2.jpeg",
+        img: "/kabbour/ProductDetails/Herbs/JouriFlowers.png",
         productId: 9,
+        category: "Natural Herbs",
         productName: "product.9.name", 
         productDesc: "product.9.desc",
         details:{
             images: [ 
-                "/kabbour/ProductDetails/jouriFlowers2.jpeg",
+                "/kabbour/ProductDetails/Herbs/JouriFlowers.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/Herbs/Rosemary.png",
+        productId: 10,
+        category: "Natural Herbs",
+        productName: "product.10.name", 
+        productDesc: "product.10.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/Rosemary.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/Herbs/EucalyptusLeaves.png",
+        productId: 11,
+        category: "Natural Herbs",
+        productName: "product.11.name", 
+        productDesc: "product.11.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/EucalyptusLeaves.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/Herbs/Chamomile.png",
+        productId: 12,
+        category: "Natural Herbs",
+        productName: "product.12.name", 
+        productDesc: "product.12.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/Chamomile.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/Herbs/ChamomileAndMint.png",
+        productId: 13,
+        category: "Natural Herbs",
+        productName: "product.13.name", 
+        productDesc: "product.13.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/Chamomile.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },
+    {
+        img: "/kabbour/ProductDetails/Herbs/WildThyme.png",
+        productId: 14,
+        category: "Natural Herbs",
+        productName: "product.14.name", 
+        productDesc: "product.14.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/WildThyme.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/Ginger.png",
+        productId: 15,
+        category: "Natural Herbs",
+        productName: "product.15.name", 
+        productDesc: "product.15.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/Ginger.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/NaturalZhourat.png",
+        productId: 16,
+        category: "Natural Herbs",
+        productName: "product.16.name", 
+        productDesc: "product.16.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/NaturalZhourat.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/MountainZhourat.png",
+        productId: 17,
+        category: "Natural Herbs",
+        productName: "product.17.name", 
+        productDesc: "product.17.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/MountainZhourat.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithGinger.png",
+        productId: 18,
+        category: "Natural Herbs",
+        productName: "product.18.name", 
+        productDesc: "product.18.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/MountainZhourat.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithGingerAndLemon.png",
+        productId: 19,
+        category: "Natural Herbs",
+        productName: "product.19.name", 
+        productDesc: "product.19.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWithGingerAndLemon.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWitthLemon.png",
+        productId: 20,
+        category: "Natural Herbs",
+        productName: "product.20.name", 
+        productDesc: "product.20.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWitthLemon.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithLemonBalm.png",
+        productId: 21,
+        category: "Natural Herbs",
+        productName: "product.21.name", 
+        productDesc: "product.21.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWithLemonBalm.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithMint.png",
+        productId: 22,
+        category: "Natural Herbs",
+        productName: "product.22.name", 
+        productDesc: "product.22.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWithMint.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithGinseng.png",
+        productId: 23,
+        category: "Natural Herbs",
+        productName: "product.23.name", 
+        productDesc: "product.23.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWithGinseng.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithCinnamonAndGinger.png",
+        productId: 24,
+        category: "Natural Herbs",
+        productName: "product.24.name", 
+        productDesc: "product.24.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWithCinnamonAndGinger.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/GreenTeaWithNaturalCardamom.png",
+        productId: 25,
+        category: "Natural Herbs",
+        productName: "product.25.name", 
+        productDesc: "product.25.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/GreenTeaWithNaturalCardamom.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/CinnamomAndGinger.png",
+        productId: 26,
+        category: "Natural Herbs",
+        productName: "product.26.name", 
+        productDesc: "product.26.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/CinnamomAndGinger.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/HibiscusWithJouriFlowers.png",
+        productId: 27,
+        category: "Natural Herbs",
+        productName: "product.27.name", 
+        productDesc: "product.27.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/HibiscusWithJouriFlowers.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/Cumin.png",
+        productId: 28,
+        category: "Natural Herbs",
+        productName: "product.28.name", 
+        productDesc: "product.28.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/Cumin.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/CuminAndLemon.png",
+        productId: 29,
+        category: "Natural Herbs",
+        productName: "product.29.name", 
+        productDesc: "product.29.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/CuminAndLemon.png",
+                "/kabbour/ProductDetails/22.png",
+            ],
+            flowerImg: "/kabbour/ProductDetails/flower.png",
+            desc:'product.9.details.desc',
+            ingredients: 'product.9.details.Ingredients',
+            usageInstructions: 'product.9.details.usageInstructions',
+            poductSpecifications: 'product.9.details.poductSpecifications'
+        }
+    },{
+        img: "/kabbour/ProductDetails/Herbs/LemonBalm.png",
+        productId: 30,
+        category: "Natural Herbs",
+        productName: "product.30.name", 
+        productDesc: "product.30.desc",
+        details:{
+            images: [ 
+                "/kabbour/ProductDetails/Herbs/LemonBalm.png",
                 "/kabbour/ProductDetails/22.png",
             ],
             flowerImg: "/kabbour/ProductDetails/flower.png",
@@ -151,4 +518,6 @@ export const ProductsData = [
             poductSpecifications: 'product.9.details.poductSpecifications'
         }
     }
+
+    
 ];
