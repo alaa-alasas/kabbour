9e18e29475a0d3e2b3d91dc6473c6b6e4f65f9c0
alaa-alasas <erp.kabbour@gmail.com>
2025-06-15 14:49:35 +0300

    add social link and update footer links map

--- src/data/FooterData.jsx
+++ src/data/FooterData.jsx
@@ -1,13 +1,11 @@
 
 export const FooterData = {
   siteMap: [
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.about.yerbaMate" },
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.about.campo" },
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.yerbaMate.products" },
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.campo.products" },
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.video" },
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.audio" },
-    { icon: "/kabbour/Icons/Vector.svg", link: "#", text: "siteMap.image.gallery" },
+    { icon: "/kabbour/Icons/Vector.svg", link: "/", text: "nav.home" },
+    { icon: "/kabbour/Icons/Vector.svg", link: "/products", text: "nav.products" },
+    { icon: "/kabbour/Icons/Vector.svg", link: "/centers", text: "nav.centers" },
+    { icon: "/kabbour/Icons/Vector.svg", link: "/events", text: "nav.media" },
+    { icon: "/kabbour/Icons/Vector.svg", link: "/about", text: "nav.about" }
   ],
   address: [
     { icon: "/kabbour/Icons/location.svg", link: "#", text: "address.yabroud" },
@@ -15,8 +13,8 @@ export const FooterData = {
     { icon: "/kabbour/Icons/email.svg", link: "#", text: "info@kabourgroup.com" },
   ],
   socialLinks: [
-    { icon: "/kabbour/Icons/facebook.svg", link: "#" },
-    { icon: "/kabbour/Icons/instagram.svg", link: "#" },
-    { icon: "/kabbour/Icons/youtube.svg", link: "#" },
+    { icon: "/kabbour/Icons/facebook.svg", link: "https://www.facebook.com/kabour.international.group/" },
+    { icon: "/kabbour/Icons/instagram.svg", link: "https://www.instagram.com/campo.syria" },
+    { icon: "/kabbour/Icons/youtube.svg", link: "https://www.youtube.com/@emadkco" },
   ],
 };
