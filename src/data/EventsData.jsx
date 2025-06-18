b71025ce78b22c61c3d37f7feaf5fb683a21bedf
alaa-alasas <erp.kabbour@gmail.com>
2025-06-18 16:00:35 +0300

    add events data

--- src/data/EventsData.jsx
+++ src/data/EventsData.jsx
@@ -1,30 +1,116 @@
 export const EventsData = [
     {
-        img: "/kabbour/Events/event1.jpg",
-        category: "1",
+        img: "/kabbour/Events/event1/3.jpg",
+        images: [
+            "/kabbour/Events/event1/1.jpg",
+            "/kabbour/Events/event1/2.jpg",
+            "/kabbour/Events/event1/3.jpg",
+            "/kabbour/Events/event1/4.jpg"
+        ],
+        category: "3",
         eventName: "event.1.name",
         eventLocation: "event.1.location",
-        eventDate: "6/2/2025" 
+        eventDate: "28/03/2023" 
     },
     {
-        img: "/kabbour/Events/event1.jpg",
-        category: "2",
+        img: "/kabbour/Events/event2/1.jpg",
+        images: [
+            "/kabbour/Events/event2/1.jpg",
+            "/kabbour/Events/event2/2.jpg",
+            "/kabbour/Events/event2/3.jpg",
+            "/kabbour/Events/event2/4.jpg"
+        ],
+        category: "3",
         eventName: "event.2.name", 
         eventLocation: "event.2.location",
-        eventDate: "6/2/2025"
+        eventDate: "20/02/2025"
     },
     {
-        img: "/kabbour/Events/event1.jpg",
-        category: "3",
+        img: "/kabbour/Events/event3/1.jpg",
+        images: [
+            "/kabbour/Events/event3/1.jpg",
+            "/kabbour/Events/event3/2.jpg",
+            "/kabbour/Events/event3/3.jpg",
+            "/kabbour/Events/event3/4.jpg"
+        ],
+        category: "2",
         eventName: "event.3.name",
         eventLocation: "event.3.location", 
-        eventDate: "6/2/2025"
+        eventDate: "20/02/2025"
     },
     {
-        img: "/kabbour/Events/event1.jpg",
-        category: "4",
+        img: "/kabbour/Events/event4/1.jpg",
+        images: [
+            "/kabbour/Events/event4/1.jpg",
+            "/kabbour/Events/event4/2.jpg",
+            "/kabbour/Events/event4/3.jpg",
+            "/kabbour/Events/event4/4.jpg"
+        ],
+        category: "3",
         eventName: "event.4.name", 
         eventLocation: "event.4.location",
-        eventDate: "6/2/2025"
+        eventDate: "19/03/2025"
+    },
+    {
+        img: "/kabbour/Events/event5/4.jpg",
+        images: [
+            "/kabbour/Events/event5/1.jpg",
+            "/kabbour/Events/event5/2.jpg",
+            "/kabbour/Events/event5/3.jpg",
+            "/kabbour/Events/event5/4.jpg"
+        ],
+        category: "1",
+        eventName: "event.5.name",
+        eventLocation: "event.5.location",
+        eventDate: "04/09/2024" 
+    },
+    {
+        img: "/kabbour/Events/event6/1.jpg",
+        images: [
+            "/kabbour/Events/event6/1.jpg",
+            "/kabbour/Events/event6/2.jpg",
+            "/kabbour/Events/event6/3.jpg",
+            "/kabbour/Events/event6/4.jpg"
+        ],
+        category: "3",
+        eventName: "event.6.name", 
+        eventLocation: "event.6.location",
+        eventDate: " 20/03/2025"
+    },
+    {
+        img: "/kabbour/Events/event7/1.jpg",
+        images: [
+            "/kabbour/Events/event7/1.jpg",
+            "/kabbour/Events/event7/2.jpg"
+        ],
+        category: "3",
+        eventName: "event.7.name",
+        eventLocation: "event.7.location", 
+        eventDate: "18/01/2024"
+    },
+    {
+        img: "/kabbour/Events/event8/1.jpg",
+        images: [
+            "/kabbour/Events/event8/1.jpg",
+            "/kabbour/Events/event8/2.jpg",
+            "/kabbour/Events/event8/3.jpg",
+            "/kabbour/Events/event8/4.jpg"
+        ],
+        category: "2",
+        eventName: "event.8.name", 
+        eventLocation: "event.8.location",
+        eventDate: "21/04/2025"
+    },
+    {
+        img: "/kabbour/Events/event9/2.jpg",
+        images: [
+            "/kabbour/Events/event9/1.jpg",
+            "/kabbour/Events/event9/2.jpg",
+            "/kabbour/Events/event9/3.jpg"
+        ],
+        category: "3",
+        eventName: "event.9.name", 
+        eventLocation: "event.9.location",
+        eventDate: "11/06/2024"
     }
 ];
